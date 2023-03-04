7777e3d1fb5429e549074788537786862e305128
Berke <93223660+quatronostro@users.noreply.github.com>
2023-03-04 17:04:18 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ venv/Q1_Python_ChatWithGPT/P02_allUpperAllLower.py
@@ -0,0 +1,13 @@
+# This project is about my Python home practice. I want to store here to see that gaining my skills in future.
+# So, I asked to ChatGPT about ask me to some Python questions about string manipulations, and I'll be answer these questions here.
+
+
+
+### QUESTION 2
+# Take string from user and convert it all uppercase version and all lowercase version.
+
+print("This program will convert text you given two versions, one is all Uppercase, other one is all Lowercase version.")
+str = input("Please enter your text : ")
+
+print("\nHere is lowercase version : ", str.lower())
+print("Here is uppercase version : ", str.upper())
---
+++ venv/Q1_Python_ChatWithGPT/P03_splitStringIntoList.py
@@ -0,0 +1,13 @@
+# This project is about my Python home practice. I want to store here to see that gaining my skills in future.
+# So, I asked to ChatGPT about ask me to some Python questions about string manipulations, and I'll be answer these questions here.
+
+
+
+### QUESTION 2
+# Take string from user split a string into a list of substrings.
+
+print("This program takes text from you, and split into a list of substrings")
+str = input("Please enter your text : ")
+
+strList = str.split()
+print("\nHere your list : ", strList)
