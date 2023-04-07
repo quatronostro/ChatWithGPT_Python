0ba3236dcee08930e28095524ab9cfe433bc00ee
Berke <93223660+quatronostro@users.noreply.github.com>
2023-04-07 22:00:31 +0300

    Q3 last version

---
+++ venv/Q3_Python_ChatGPT/P01_listToSquare.py
@@ -0,0 +1,16 @@
+# This project is about my Python home practice. I want to store here to see that gaining my skills in the future.
+# So, I asked to ChatGPT about ask me to some Python questions, and I'll be answer these questions here.
+
+
+
+### QUESTION 1
+# Create a list called my_list that contains the values [1, 2, 3, 4, 5]. Using a for loop,
+# print out the square of each number in the list.
+
+
+
+my_list = [1, 2, 3, 4, 5]
+
+for i in my_list:
+    print(i * i)
+
